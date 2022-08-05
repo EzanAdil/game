43b0c28797405b244201c2fa80d79f38222b810c
EzanAdil <std.ezankhan@gmail.com>
2022-08-04 20:44:39 -0700

    fighting game

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+var count = 350;
+window.onkeydown = function () {
+  console.log(event.keyCode);
+  var char = document.getElementById("char");
+  if (event.keyCode === 39) {
+    char.src = "two.gif";
+    count = count + 50;
+    console.log(count);
+    char.style.left = count + "px";
+    setTimeout(function () {
+      char.src = "";
+    }, 100);
+  }
+  if (event.keyCode === 37) {
+    character.src = "two.gif";
+    count = count - 50;
+    console.log(count);
+    character.style.left = count + "px";
+    setTimeout(function () {
+      character.src = "one.gif";
+    }, 100);
+  }
+  if (event.keyCode === 65) {
+    character.src = "three.gif";
+    setTimeout(function () {
+      character.src = "one.gif";
+    }, 1000);
+  }
+};
